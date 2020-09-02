62ccd63443194c748dd25a103b92c6fd1f20af54
Onore849 <rion3376@gmail.com>
2020-09-02 16:17:55 +0900

    download image by sdwebImage

--- DownloadImage/ViewController.swift
+++ DownloadImage/ViewController.swift
@@ -5,12 +5,12 @@
 //  Created by 野澤拓己 on 2020/09/02.
 //  Copyright © 2020 Takumi Nozawa. All rights reserved.
 //
-
+import SDWebImage
 import UIKit
 
 class ViewController: UIViewController {
     
-    let urlString = "https://iosacademy.io/assets/images/brand/header.png"
+    let urlString = "https://iosacademy.io/assets/images/brand/icon.jpg"
     
     private let imageView: UIImageView = {
        
@@ -27,32 +27,39 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
         imageView.center = view.center
         
-        fetchImage()
+//        fetchImage()
+        
+        // sdwebImageを使った画像ダウンロード 関数: fetchImageがこの一行で終わる
+//        imageView.sd_setImage(with: URL(string: urlString), completed: nil)
+        imageView.sd_setImage(with: URL(string: urlString),
+                              placeholderImage: UIImage(systemName: "flame"),
+                              options: .continueInBackground,
+                              completed: nil)
     }
     
-    private func fetchImage() {
-        // get data
-        // convert the data to image
-        // set image to image View
-        guard let url = URL(string: urlString) else {
-            return
-        }
-        
-        let getDataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
-            
-            guard let data = data, error == nil else {
-                return
-            }
-            
-            DispatchQueue.main.async {
-                let image = UIImage(data: data)
-                self.imageView.image = image
-            }
-        }
-        
-        getDataTask.resume()
-        
-    }
+//    private func fetchImage() {
+//        // get data
+//        // convert the data to image
+//        // set image to image View
+//        guard let url = URL(string: urlString) else {
+//            return
+//        }
+//
+//        let getDataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+//
+//            guard let data = data, error == nil else {
+//                return
+//            }
+//
+//            DispatchQueue.main.async {
+//                let image = UIImage(data: data)
+//                self.imageView.image = image
+//            }
+//        }
+//
+//        getDataTask.resume()
+//
+//    }
     
 
 }
