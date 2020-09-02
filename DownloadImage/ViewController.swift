5413bb2d645b69da79f015efc03a9015bdb901a7
Onore849 <rion3376@gmail.com>
2020-09-02 16:08:01 +0900

    first commit

--- DownloadImage/ViewController.swift
+++ DownloadImage/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let urlString = "https://iosacademy.io/assets/images/brand/header.png"
+    
+    private let imageView: UIImageView = {
+       
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(imageView)
+        imageView.center = view.center
+        
+        fetchImage()
     }
-
+    
+    private func fetchImage() {
+        // get data
+        // convert the data to image
+        // set image to image View
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let getDataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                self.imageView.image = image
+            }
+        }
+        
+        getDataTask.resume()
+        
+    }
+    
 
 }
 
